1022b6fc9f10a79df9494b318ed19aad9ad6e1cf
YUKIT4N <marcinww18@wp.pl>
2016-04-06 01:00:12 +0200

    Added files via upload

--- src/WojnaSwiatow.java
+++ src/WojnaSwiatow.java
@@ -1,66 +1,91 @@
-import javax.swing.JFrame;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import javax.swing.JPanel;
-import java.util.HashMap;
 import java.awt.Canvas;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.net.URL;
-import javax.imageio.ImageIO;
-public class WojnaSwiatow extends Canvas{
-	
-	public static final int SZEROKOSC = 800;
-	public static final int WYSOKOSC = 600;
-	public HashMap sprites;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.util.ArrayList;
 
-	public WojnaSwiatow() {
-		sprites = new HashMap();
-		JFrame okno = new JFrame(".:Wojna Swiatow:.");
-		JPanel panel = (JPanel)okno.getContentPane();
-		setBounds(0,0,SZEROKOSC,WYSOKOSC);
-		panel.setPreferredSize(new Dimension(SZEROKOSC,WYSOKOSC));
-		panel.setLayout(null);
-		panel.add(this);
-		okno.setBounds(0,0,SZEROKOSC,WYSOKOSC);
-		okno.setVisible(true);
-		okno.addWindowListener( new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				System.exit(0);
-			}
-		});
-	}
-	
-	public void paint(Graphics g){
-		g.drawImage(getSprite("potworek.jpg"), 40, 40,this);
-	}
-	
-	public static void main(String[] args) {
-		WojnaSwiatow inv = new WojnaSwiatow();
-	}
-	
-	public BufferedImage loadImage(String url) {
-		try {
-			return ImageIO.read(new File(url));
+public class WojnaSwiatow extends Canvas implements Stage{
+	public long usedTime;
+	public BufferStrategy strategia;
+	private SpriteCache spriteCache;
+	private ArrayList actors;
+public WojnaSwiatow() {
+	spriteCache = new SpriteCache();
+	JFrame okno = new JFrame(".: Wojna Swiatow :.");
+	JPanel panel = (JPanel)okno.getContentPane();
+	setBounds(0,0,Stage.SZEROKOSC,Stage.WYSOKOSC);
+	panel.setPreferredSize(new Dimension(Stage.SZEROKOSC,Stage.WYSOKOSC));
+	panel.setLayout(null);
+	panel.add(this);
+	okno.setBounds(0,0,Stage.SZEROKOSC,Stage.WYSOKOSC);
+	okno.setVisible(true);
+	okno.addWindowListener( new WindowAdapter() {
+	public void windowClosing(WindowEvent e) {
+	System.exit(0);
 		}
-		catch (Exception e) {
-			System.out.println("Przy otwieraniu " +" jako " + url);
-			System.out.println("Wystapil blad : "+e.getClass().getName()+" "+e.getMessage());
-			System.exit(0);
-			return null;
-		}
-	}
-	
-	public BufferedImage getSprite(String sciezka) {
-		BufferedImage img = (BufferedImage)sprites.get(sciezka);
-		if (img == null) {
-			img = loadImage("img/"+sciezka);
-			sprites.put(sciezka,img);
-		}
-		return img;
-	}
-//yo
+	});
+	okno.setResizable(false);
+	createBufferStrategy(2);
+	strategia = getBufferStrategy();
+	requestFocus();
 }
+public void initWorld() {
+	actors = new ArrayList();
+	for (int i = 0; i < 10; i++){
+	Monster m = new Monster(this);
+	m.setX( (int)(Math.random()*Stage.SZEROKOSC) );
+	m.setY( i*20 );
+	m.setVx( (int)(Math.random()*3)+1 );
+	actors.add(m);
+	}
+	}
+	public void paintWorld() {
+	Graphics2D g = (Graphics2D)strategia.getDrawGraphics();
+	g.setColor(Color.black);
+	g.fillRect(0,0,getWidth(),getHeight());
+	for (int i = 0; i < actors.size(); i++) {
+	Actor m = (Actor)actors.get(i);
+	m.paint(g);
+	}
+	g.setColor(Color.white);
+	if (usedTime > 0)
+	g.drawString(String.valueOf(1000/usedTime)+" fps",0,Stage.WYSOKOSC-50);
+	else
+	g.drawString("--- fps",0,Stage.WYSOKOSC-50);
+	strategia.show();
+	}
+	public void updateWorld() {
+	for (int i = 0; i < actors.size(); i++) {
+	Actor m = (Actor)actors.get(i);
+	m.act();
+	}
+	}
+	public SpriteCache getSpriteCache() {
+	return spriteCache;
+	}
+	public void game() {
+	usedTime=1000;
+	initWorld();
+
+	while (isVisible()) {
+		long startTime = System.currentTimeMillis();
+		updateWorld();
+		paintWorld();
+		usedTime = System.currentTimeMillis()-startTime;
+		try {
+		Thread.sleep(Stage.SZYBKOSC);
+		} catch (InterruptedException e) {}
+		}
+		}
+		public static void main(String[] args) {
+		WojnaSwiatow inv = new WojnaSwiatow();
+		inv.game();
+		}
+		}
+//hell
